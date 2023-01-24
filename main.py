7f95a6e36c571d9bf8f92e454ea0f48e513991ed
hardishc <contacthardish@gmail.com>
2023-01-24 13:49:25 -0700

    Get price from a link

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # selenium 4
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.firefox import GeckoDriverManager
 
 options = FirefoxOptions()
 driver = webdriver.Firefox(options=options)
 
-driver.get("https://www.hollisterco.com/shop/ca/p/oversized-spider-man-graphic-hoodie-47815825?seq=01&categoryId=12635&faceout=model")
+driver.get("https://www.amazon.ca/COCHOA-Triple-Leather-Womens-Crossbody/dp/B07RY8JQ1D?ref_=Oct_DLandingS_D_de9c8009_62")
+
+price = driver.find_element(By.XPATH, "//*[contains(@class, 'Price') or contains(@class, 'price') or contains(@class, 'Pricing') or contains(@class, 'pricing')]").text
+
+print(price)
+
+driver.quit()
