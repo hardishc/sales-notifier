88924a7e282bcdd2b6fd86c2903e1aec8c6bd6fa
hardishc <contacthardish@gmail.com>
2023-01-30 16:29:25 -0700

    Iterate over csv and get price

--- main.py
+++ main.py
@@ -1,29 +1,50 @@
 # selenium 4
 from selenium import webdriver
+import re
 from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.firefox import GeckoDriverManager
+
+
+def current_price(file):
+    for item in file:
+        comma_index = item.index(',')
+        item_url = item[0:comma_index]
+
+        if item_url == "url":
+            continue
+
+        item_price = item[comma_index+1:]
+
+        driver.get(item_url)        
+
+        new_price = driver.find_elements(By.XPATH, "//*[contains(@class, 'Price') or contains(@class, 'price') or contains(@class, 'Pricing') or contains(@class, 'pricing') or contains(@class, 'money') or contains(@class, 'Money')]")
+
+        for i in new_price:            
+            x = re.search("([0-9]+\.[0-9].)", i.text) or re.search("([0-9]+)", i.text)
+            print(x.group())            
+            break
+            
+
+        
+
+
 
 options = FirefoxOptions()
 #options.headless = True
 options.page_load_strategy = 'eager'
 driver = webdriver.Firefox(options=options)
 
-#driver.get("https://www.garageclothing.com/ca/p/perfect-puffer-matte-jacket/0796161.html?&&utm_campaign=grg-perf-max-can-en&utm_source=google&utm_medium=cpc&utm_term=-&gclid=EAIaIQobChMIzrSf3Zzh_AIVem1vBB1BewuAEAYYASABEgI6pPD_BwE")
-#driver.get("https://www.amazon.ca/gp/product/B09DPLXHKH/ref=ox_sc_act_title_1?smid=ASWMM7BDGBZWM&psc=1")
-#driver.get("https://www2.hm.com/en_ca/productpage.0875217034.html")
-#driver.get("https://www.hollisterco.com/shop/ca/p/curvy-high-rise-flare-jeans-50419711?seq=06&categoryId=12552&faceout=model")
-#driver.get("https://www.abercrombie.com/shop/ca/p/mixed-fabric-curve-love-ultra-high-rise-ankle-straight-jean-51389870?seq=29&categoryId=12261&faceout=model")
-#driver.get("https://oldnavy.gapcanada.ca/browse/product.do?pid=506760013&cid=1185415&pcid=1031099&vid=1#pdp-page-content")
+file = open("price.csv", "r")
+
+current_price(file)
+
 #driver.get("https://www.ikea.com/ca/en/p/knarrevik-nightstand-black-30381183/")
-driver.get("https://www.zara.com/ca/en/trf-mom-fit-comfort-jeans-p08197031.html?v1=222770189&v2=2187589")
+
+
 
 sleep(4)
-price = driver.find_elements(By.XPATH, "//*[contains(@class, 'Price') or contains(@class, 'price') or contains(@class, 'Pricing') or contains(@class, 'pricing') or contains(@class, 'money') or contains(@class, 'Money')]")
-
-for i in price:
-    print(i.text)
-    break
+#price = driver.find_elements(By.XPATH, "//*[contains(@class, 'Price') or contains(@class, 'price') or contains(@class, 'Pricing') or contains(@class, 'pricing') or contains(@class, 'money') or contains(@class, 'Money')]")
 
 driver.quit()
